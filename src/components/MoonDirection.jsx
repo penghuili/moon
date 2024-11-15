3fdedb28d06957c8968beaba55f94d43c887f188
Peng <peng37@tutanota.com>
2024-11-15 21:26:36 +0100

    remove device direction

--- src/components/MoonDirection.jsx
+++ src/components/MoonDirection.jsx
@@ -1,42 +1,33 @@
-import React, { useEffect, useMemo, useState } from 'react';
+import { RiArrowUpFill } from '@remixicon/react';
+import React, { useMemo } from 'react';
 import fastMemo from 'react-fast-memo';
 import { useCat } from 'usecat';
 
 import { moonDataCat } from '../store/moonCats.jsx';
 
 export const MoonDirection = fastMemo(() => {
-  const [direction, setDirection] = useState(0);
   const moonData = useCat(moonDataCat);
 
-  const compassDirection = useMemo(() => {
+  const moonDirection = useMemo(() => {
     if (!moonData.azimuth) {
       return 0;
     }
 
-    const azimuthDegrees = (moonData.azimuth * 180) / Math.PI;
+    const azimuthDegrees = (moonData.azimuth * 180) / Math.PI - 180;
     return (azimuthDegrees + 360) % 360;
   }, [moonData.azimuth]);
-
-  useEffect(() => {
-    const handleOrientation = event => {
-      setDirection(event.alpha || 0); // alpha represents compass direction in degrees
-    };
-    window.addEventListener('deviceorientation', handleOrientation);
-    return () => window.removeEventListener('deviceorientation', handleOrientation);
-  }, []);
 
   return (
     <div>
       <h2 style={{ margin: '3rem 0 0' }}>Direction</h2>
       <div
         style={{
-          transform: `rotate(${direction - compassDirection}deg)`,
+          transform: `rotate(${moonDirection}deg)`,
           transition: 'transform 0.5s',
-          fontSize: '3em',
           marginTop: '20px',
         }}
       >
-        ➡️
+        <RiArrowUpFill size={80} color="#FFC850" />
       </div>
     </div>
   );
