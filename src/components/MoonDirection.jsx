94a04c7618461c71cd2ea507387cd16e2b95b935
Peng <peng37@tutanota.com>
2024-11-15 21:35:24 +0100

    remove device

--- src/components/MoonDirection.jsx
+++ src/components/MoonDirection.jsx
@@ -1,13 +1,11 @@
 import { RiArrowUpFill } from '@remixicon/react';
-import React, { useEffect, useMemo, useState } from 'react';
+import React, { useMemo } from 'react';
 import fastMemo from 'react-fast-memo';
 import { useCat } from 'usecat';
 
 import { moonDataCat } from '../store/moonCats.jsx';
 
 export const MoonDirection = fastMemo(() => {
-  const [deviceDirection, setDeviceDirection] = useState(0);
-
   const moonData = useCat(moonDataCat);
 
   const moonDirection = useMemo(() => {
@@ -19,21 +17,12 @@ export const MoonDirection = fastMemo(() => {
     return (azimuthDegrees + 360) % 360;
   }, [moonData.azimuth]);
 
-  useEffect(() => {
-    const handleOrientation = event => {
-      const clockwiseDegrees = 360 - (event.alpha || 0);
-      setDeviceDirection(clockwiseDegrees);
-    };
-    window.addEventListener('deviceorientation', handleOrientation);
-    return () => window.removeEventListener('deviceorientation', handleOrientation);
-  }, []);
-
   return (
     <div>
       <h2 style={{ margin: '3rem 0 0' }}>Direction</h2>
       <div
         style={{
-          transform: `rotate(${moonDirection - deviceDirection}deg)`,
+          transform: `rotate(${moonDirection}deg)`,
           transition: 'transform 0.5s',
           marginTop: '20px',
         }}
